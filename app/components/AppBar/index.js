57bfa974db7fbf4e3e4764cd2334d825a8b5a866
Sandy Zhang <sandy@flamingo.io>
2018-01-01 11:20:04 +1100

    style the components

---
+++ app/components/AppBar/index.js
@@ -0,0 +1,43 @@
+/**
+*
+* AppBar
+*
+*/
+
+import React from 'react';
+
+
+import styles from './styles.css';
+import FontAwesome from 'react-fontawesome';
+
+function AppBar({ toggleDrawer }) {
+  return (
+    <div className={styles.appBar}>
+      <div
+        className={styles.iconButton}
+        onClick={toggleDrawer}
+      >
+        <FontAwesome
+          className={styles.icon}
+          name="bars"
+        />
+      </div>
+      <div
+        className={styles.heading}
+      >
+        Coder daily
+      </div>
+      <div
+        className={styles.linkContainer}
+      >
+        log in
+      </div>
+    </div>
+  );
+}
+
+AppBar.propTypes = {
+  toggleDrawer: React.PropTypes.func.isRequired,
+};
+
+export default AppBar;
--- app/containers/NavigationContainer/reducer.js
+++ app/containers/NavigationContainer/reducer.js
@@ -8,10 +8,12 @@ import { fromJS } from 'immutable';
 import {
   REQUEST_TOPICS_SUCCEEDED,
   SELECT_TOPIC,
+  TOGGLE_DRAWER,
 } from './constants';
 
 const initialState = fromJS({
   topics: [],
+  isDrawerOpen: false,
 });
 
 function navigationContainerReducer(state = initialState, action) {
@@ -19,7 +21,9 @@ function navigationContainerReducer(state = initialState, action) {
     case REQUEST_TOPICS_SUCCEEDED:
       return state.set('topics', action.topics);
     case SELECT_TOPIC:
-      return state.set('selectTopic', action.topic);
+      return state.set('selectTopic', action.topic).set('isDrawerOpen', false);
+    case TOGGLE_DRAWER:
+      return state.set('isDrawerOpen', !state.get('isDrawerOpen'));
     default:
       return state;
   }
